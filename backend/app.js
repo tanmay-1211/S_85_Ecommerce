97c9e97b4a0bb697fc7eb8e02f8a7de608e3fb58
tanmay-1211 <tanmayskbhatt@gmail.com>
2025-02-13 14:59:19 +0530

    fifth

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ const app=express()
 const cookieParser=require("cookie-parser")
 const bodyParser=require("body-parser")
 
-
+const cors=require("cors")
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use("/" ,express.static("uploads"))
