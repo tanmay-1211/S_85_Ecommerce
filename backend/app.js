fc1000ab7a96fa16e2bf7af7f7c390305f80526d
tanmay-1211 <tanmayskbhatt@gmail.com>
2025-02-17 12:01:11 +0530

    tenth

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const ErrorHandler = require('./utils/ErrorHandler'); 
+const ErrorHandler = require("./utils/ErrorHandler");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const cors = require("cors")
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", express.static("uploads"));
@@ -13,7 +13,9 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "./config/.env" });
 }
 const user = require("./controller/user");
+const product=require("./controller/product")
 app.use("/api/v2/user", user);
+app.use("/api/v2/product",product)
 app.use(ErrorHandler);
 
 module.exports = app;
