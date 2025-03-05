79012214522779d0c1cd66f58c01dadc91723fa2
tanmay-1211 <tanmayskbhatt@gmail.com>
2025-03-05 09:44:58 +0530

    Milestone-16

--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -5,6 +5,7 @@ import Home from "../src/pages/Home.jsx"
 import CreateProduct from "../src/pages/createProduct.jsx"
 import MyProducts from "../src/pages/myProducts.jsx";
 import Cart from "./pages/cart.jsx";
+import ProductDetails from "./pages/productDetails.jsx";
 export
 {
     LoginPage,
@@ -13,7 +14,8 @@ export
     Home,
     CreateProduct,
     MyProducts,
-    Cart
+    Cart,
+    ProductDetails
 
 }
 
